80817102d0fa852a0bc0515c352d271270da52c7
Magnús Skúlason <maggisk@gmail.com>
2019-10-03 08:06:14 +0000

    wip moving between computers

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+local Object = require "classic"
 local Animation = require "animation"
 local util = require "util"
 local ui = require "ui"
@@ -76,6 +77,239 @@ local function rotate(x, y, centerX, centerY)
   fl.angle = state.rotation.angle + (now - start)
 end
 
+local Element = Object:extend()
+function Element:new(attr, ...)
+  self.attr = attr or {}
+  self.children = {...}
+  self.events = {}
+end
+
+function Element:bind(method)
+  return function(...)
+    return method(self, ...)
+  end
+end
+
+function Element:add(...)
+  for _, child in ipairs({...}) do
+    table.insert(self.children, child)
+  end
+end
+
+function Element:dispatcher(name, payload)
+  return function(element)
+    local root = self
+    while root.parent do root = root.parent end
+    root.attr.dispatch(name, payload)
+  end
+end
+
+function Element:iter()
+  local r, h, t, q = nil, 1, 1, {self}
+  return function()
+    if h > t then return end
+    for _, child in ipairs(q[h].children) do
+      t = t + 1
+      q[t] = child
+    end
+    r, q[h] = q[h], nil
+    h = h + 1
+    return r
+  end
+end
+
+local function matches(node, attr)
+  for k, v in pairs(attr) do
+    if node.attr[k] ~= v then return false end
+  end
+  return true
+end
+
+function Element:query(attr)
+  local r = {}
+  for node in self:iter() do
+    if matches(node, attr) then
+      r[#r+1] = node
+    end
+  end
+  return r
+end
+
+function Element:on(name, fn)
+  assert(name and fn)
+  self.events[name] = self.events[name] or {}
+  table.insert(self.events[name], fn)
+  return self
+end
+
+function Element:trigger(name, e)
+  for _, fn in ipairs(self.events[name] or {}) do
+    fn(e)
+  end
+end
+
+function Element:handleEvent(name, e)
+  for _, child in ipairs(self.children) do
+    if child:handleEvent(name, e) == false then
+      return false
+    end
+  end
+  return self:trigger(name, e)
+end
+
+function Element:processMouseEvent(name, e)
+  if self:contains(e.x, e.y) then
+    for _, child in ipairs(self.children) do
+      if child:triggerMouseEvent(e) == false then
+        return false
+      end
+    end
+  end
+end
+
+function Element:contains(x, y)
+  if not self.width or not self.height then return true end -- dummy wrapper
+  return (self.x <= x and x <= self.x + self.width and self.y <= y and y <= self.y + self.height)
+end
+
+function Element:render(options)
+  self.visible = true -- invisible elements don't get event callbacks because... they are not visible
+  options = options or {}
+  util.extend(self.attr, options.attr)
+  self.x, self.y = love.graphics.transformPoint(0, 0)
+  self:draw()
+  self.width = self.width or self.attr.width or 0
+  self.height = self.height or self.attr.height or 0
+
+  if options.layout then
+    love.graphics.translate(options.layout.x * self.attr.width, options.layout.y * self.attr.height)
+  end
+end
+
+local Text = {}
+function Text.getDimensions(text, size)
+  local font = util.getFont(size)
+  local width = font:getWidth(text)
+  return width, font:getHeight()
+end
+
+function Text.print(text, size, x, y)
+  love.graphics.setFont(util.getFont(size))
+  love.graphics.print(text, x or 0, y or 0)
+end
+
+local function setdefault(t, k, v)
+  if not t[k] then t[k] = v end
+end
+
+local Button = Element:extend()
+function Button:new(attr)
+  Element.new(self, attr)
+  setdefault(self.attr, 'paddingX', 10)
+  setdefault(self.attr, 'paddingY', 5)
+  setdefault(self.attr, 'fontsize', 20)
+
+  self:on('__mousemoved', function(x, y)
+    self.hovering = self:contains(x, y)
+  end)
+end
+
+function Button:draw()
+  local tw, th = Text.getDimensions(self.attr.text, self.attr.fontsize)
+  self.width = self.attr.width or tw + (self.attr.paddingX or 0) * 2
+  self.height = self.attr.height or th + (self.attr.paddingY or 0) * 2
+  love.graphics.setColor(self.attr.background)
+  love.graphics.rectangle("fill", 0, 0, self.width, self.height)
+  local x = self.width / 2 - tw / 2
+  local y = self.height / 2 - th / 2
+  if self.attr.align == "left" then
+    x = self.attr.paddingX
+  elseif self.attr.align == "right" then
+    x = self.width - self.paddingX - th
+  end
+  love.graphics.setColor(self.attr.color)
+  Text.print(self.attr.text, self.attr.fontsize, x, y)
+end
+
+local dir = {
+  right = {x = -1, y = 0},
+  left = {x = 1, y = 0},
+  up = {x = 0, y = -1},
+  down = {x = 0, y = 1},
+}
+
+local Header = Element:extend()
+function Header:new(attr, state)
+  Element.new(self, attr)
+  self.state = state
+  self.tab = 'file'
+
+  local h = 30
+  local function setTab(tab) self.tab = tab.attr.id end
+  Element.new(self, attr,
+    -- main tabs
+    Button({id = 'file', type = 'main', text = "File"}):on('mousepressed', setTab),
+    Button({id = 'edit', type = 'main', text = "Edit"}):on('mousepressed', setTab),
+    Button({id = 'layer', type = 'main', text = "Layer"}):on('mousepressed', setTab),
+    Button({id = 'frame', type = 'main', text = "Frame"}):on('mousepressed', setTab),
+
+    -- file subtabs
+    Button(fileTab, {text = "Save"}):on('mousepressed', self:dispatcher('SAVE')),
+
+    -- edit subtabs
+    Button({text = 'Undo', under = 'file'}),
+    Button({text = 'Redo', under = 'file'}),
+
+    -- TODO: layer subtabs - delete etc.
+
+    -- frame subtabs
+    Button({id = 'easing', under = 'frame', text = ''}):on('mousepressed', self:dispatcher('CHOOSE_EASING')),
+    Button({under = 'frame', text = 'move left'}):on('mousepressed', self:dispatcher('MOVE_FRAME', {by = -1})),
+    Button({under = 'frame', text = 'move right'}):on('mousepressed', self:dispatcher('MOVE_FRAME', {by = 1})),
+    Button({under = 'frame', text = 'add'}):on('mousepressed', self:dispatcher('NEW_FRAME')),
+    Button({under = 'frame', text = 'delete'}):on('mousepressed', self:dispatcher('DELETE_FRAME'))
+  )
+
+  self:on('resize', function(e)
+    self.attr.width = e.width
+  end)
+end
+
+local function whereami()
+  return love.graphics.transformPoint(0, 0)
+end
+
+local function move(m)
+  if m.to then
+    local x, y = whereami()
+    love.graphics.translate(m.to.x or x, m.to.y or y)
+  end
+  if m.by then
+    local x, y = whereami()
+    love.graphics.translate(x + (m.by.x or 0), y + (m.by.y or 0))
+  end
+end
+
+function Header:draw()
+  self:query({id = 'easing'})[1].attr.text = self.state.frame.easing
+  local x = layout.sidebar.width
+  local h = 30
+
+  move({to = {x = self.x + layout.sidebar.width, y = self.y + self.attr.height - 30}})
+  for _, tab in ipairs(self:query({under = self.tab})) do
+    tab:render({layout = dir.right, attr = {height = h}})
+  end
+
+  move({by = {y = -h - 3}, to = {x = x}})
+  love.graphics.setColor(colors.button.selected.background)
+  love.graphics.rectangle("fill", -1000, 0, 100000, 3)
+
+  move({by = {y = -h}})
+  for _, tab in ipairs(self:query({type = 'main'})) do
+    tab:render({layout = dir.right})
+  end
+end
+
 -- ui layout
 local sidebar = ui.Area(state.ui, {y = layout.header.height})
 local header = ui.Area(state.ui)
@@ -93,10 +327,10 @@ local play = ui.Area(state.ui, {w = 50, h = 50}):on('mousepressed', function()
 end)
 
 -- main tabs
-local fileTab = newButton(header, {text = "File"}):on('mousepressed', setTab)
-local editTab = newButton(header, {text = "Edit"}):on('mousepressed', setTab)
-local layerTab = newButton(header, {text = "Layer"}):on('mousepressed', setTab)
-local frameTab = newButton(header, {text = "Frame"}):on('mousepressed', setTab)
+local fileTab = newButton(header, {id = 'file', type = 'maintab', text = "File"}):on('mousepressed', setTab)
+local editTab = newButton(header, {id = 'edit', type = 'maintab', text = "Edit"}):on('mousepressed', setTab)
+local layerTab = newButton(header, {id = 'layer', type = 'maintab', text = "Layer"}):on('mousepressed', setTab)
+local frameTab = newButton(header, {id = 'frame', type = 'maintab', text = "Frame"}):on('mousepressed', setTab)
 local mainTabs = {fileTab, editTab, layerTab, frameTab}
 state.tab = fileTab
 
@@ -365,15 +599,18 @@ function love.load(arg)
   end
 end
 
+local header
 function love.draw()
   love.graphics.clear(colors.background)
-  state.ui:updateAll({visible = false})
-
-  for _, name in ipairs({"header", "footer", "sidebar", "animationArea", "easingPicker"}) do
-    love.graphics.reset()
-    love.graphics.origin()
-    draw[name]()
-  end
+  header = header or Header({width = 800, height = 100}, state)
+  header:render()
+  -- state.ui:updateAll({visible = false})
+  --
+  -- for _, name in ipairs({"header", "footer", "sidebar", "animationArea", "easingPicker"}) do
+  --   love.graphics.reset()
+  --   love.graphics.origin()
+  --   draw[name]()
+  -- end
 end
 
 function love.update(dt)
